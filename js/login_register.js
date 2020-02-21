71ef29ab16511e9e01e1599837ad21f187b4e0ff
adrianruizmartinez <43651863+adrianruizmartinez@users.noreply.github.com>
2020-02-21 09:41:26 +0100

    Add files via upload

--- js/login_register.js
+++ js/login_register.js
@@ -1,15 +1,15 @@
 $(function () {
 
     $('#login-form-link').click(function (e) {
-        $("#login-form").delay(100).fadeIn(100);
-        $("#register-form").fadeOut(100);
+        $("#login-form").delay(100).show();
+        $("#register-form").delay(100).hide();
         $('#register-form-link').removeClass('active');
         $(this).addClass('active');
         e.preventDefault();
     });
     $('#register-form-link').click(function (e) {
-        $("#register-form").delay(100).fadeIn(100);
-        $("#login-form").fadeOut(100);
+        $("#register-form").delay(100).show();
+        $("#login-form").delay(100).hide();
         $('#login-form-link').removeClass('active');
         $(this).addClass('active');
         e.preventDefault();
